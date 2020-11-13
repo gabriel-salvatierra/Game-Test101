ee5d4c59344db8a1e4aa603cb495991ceb89f518
angelsalvatierra <angel.salvatierra@globallogic.com>
2020-11-13 02:27:15 -0300

    Game and Info

---
+++ Assets/Scripts/FantasyGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FantasyGame : MonoBehaviour {
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start() {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ManageState();
+        QuitGame();
+    }
+
+    private void QuitGame() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            Application.Quit();
+        }
+    }
+
+    private void ManageState() {
+        var nextStates = state.GetNextStates();
+        for (int index = 0; index < nextStates.Length; index++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) {
+                state = nextStates[index];
+            }
+        }
+
+        /*       if (Input.GetKeyDown(KeyCode.Alpha1)) {
+                   state = nextStates[0];
+               } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+                   state = nextStates[1];
+               } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+                   state = nextStates[2];
+               }
+       */
+
+        textComponent.text = state.GetStateStory();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Test101
+
+Text based game from a **GameDev.tv** course
+
+### Instructions to download and play
+
+1. Click `Game` folder
+2. Click `Test101Game.zip`
+3. Select `Download`
+4. Unzip file
+5. In unzipped folder doubleclick `Test101.exe`
+6. Enjoy!
+
+![MenuImage](Demo/MenuImage.PNG)
